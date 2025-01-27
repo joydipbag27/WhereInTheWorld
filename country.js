0cae1fd7145251f30bb6078def63ab57b32e2c04
joydipbag27 <79678328+joydipbag27@users.noreply.github.com>
2025-01-27 17:29:40 +0530

    Add files via upload

---
+++ country.js
@@ -0,0 +1,95 @@
+const flag = document.querySelector(".flag");
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+const countryName = document.querySelector(".country-name");
+const nativeName = document.querySelector(".native-name > span");
+const population = document.querySelector(".population > span");
+const region = document.querySelector(".region > span");
+const subRegion = document.querySelector(".sub-region > span");
+const capital = document.querySelector(".capital > span");
+const topDomain = document.querySelector(".top-domain > span");
+const currencies = document.querySelector(".currencies > span");
+const languages = document.querySelector(".languages > span");
+const borderCountries = document.querySelector(".border-countries-container");
+const backBtn = document.querySelector(".back-btn");
+
+
+
+
+if(localStorage.getItem('darkMode') == 'true'){
+  document.body.classList.add('dark-mode')
+  darkModeBtn.innerHTML = `<i class="ri-sun-line"></i> Light Mode`
+}else{
+  darkModeBtn.innerHTML = '<i class="ri-moon-line"></i> Dark Mode'
+}
+
+darkModeBtn.addEventListener('click', () =>{
+  document.body.style.transition = 'all 0.5s'
+  document.body.classList.toggle('dark-mode')
+  if(document.body.classList.contains('dark-mode')){
+    darkModeBtn.innerHTML = `<i class="ri-sun-line"></i> Light Mode`
+    localStorage.setItem('darkMode', true)
+  }else{
+    document.body.style.transition = 'all 0.5s'
+    darkModeBtn.innerHTML = '<i class="ri-moon-line"></i> Dark Mode'
+    localStorage.setItem('darkMode', false)
+  }
+})
+
+let allData;
+const urlSplit = "%20";
+let countryURLName = new URLSearchParams(location.search)
+  .get("name")
+  .toLocaleLowerCase()
+  .replace(" ", urlSplit);
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    allData = data;
+  });
+
+fetch(`https://restcountries.com/v3.1/name/${countryURLName}?fullText=true`)
+  .then((res) => res.json())
+  .then((data) => {
+    flag.src = data[0].flags.svg;
+    countryName.textContent = data[0].name.common;
+    try {
+      nativeName.textContent = Object.values(data[0].name.nativeName)[
+        length
+      ].common;
+    } catch (err) {
+      console.log(`did not find value`);
+    }
+    population.textContent = data[0].population.toLocaleString("en-IN");
+    region.textContent = data[0].region;
+    subRegion.textContent = data[0].subregion;
+    capital.textContent = data[0].capital;
+    topDomain.textContent = data[0].tld;
+    try {
+      currencies.textContent = Object.values(data[0].currencies)[0].name;
+    } catch (err) {
+      console.log(`did not find value`);
+    }
+    try {
+      languages.textContent = Object.values(data[0].languages).join(", ");
+    } catch (err) {
+      console.log(`did not find value`);
+    }
+
+    if (data[0].borders) {
+      data[0].borders.forEach((con) => {
+        allData.forEach((search, index) => {
+          if (con == search.cca3) {
+            const borderCountry = document.createElement("a");
+            borderCountry.innerText = allData[index].name.common;
+            borderCountry.href = `/WhereInTheWorld/country.html?name=${allData[index].name.common}`;
+            borderCountries.appendChild(borderCountry);
+          }
+        });
+      });
+    }
+  });
+
+backBtn.addEventListener("click", () => {
+  window.history.back();
+});
---
+++ script.js
@@ -0,0 +1,87 @@
+const main = document.querySelector("main");
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+const filter = document.querySelector(".filter");
+const search = document.querySelector('input')
+
+
+
+if(localStorage.getItem('darkMode') == 'true'){
+  document.body.classList.add('dark-mode')
+  darkModeBtn.innerHTML = `<i class="ri-sun-line"></i> Light Mode`
+}else{
+  darkModeBtn.innerHTML = '<i class="ri-moon-line"></i> Dark Mode'
+}
+
+darkModeBtn.addEventListener('click', () =>{
+  document.body.style.transition = 'all 0.5s'
+  document.body.classList.toggle('dark-mode')
+  if(document.body.classList.contains('dark-mode')){
+    darkModeBtn.innerHTML = `<i class="ri-sun-line"></i> Light Mode`
+    localStorage.setItem('darkMode', true)
+  }else{
+    document.body.style.transition = 'all 0.5s'
+    darkModeBtn.innerHTML = '<i class="ri-moon-line"></i> Dark Mode'
+    localStorage.setItem('darkMode', false)
+  }
+})
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const countriesCard = document.createElement("a");
+      countriesCard.classList.add("countries-card");
+      countriesCard.href = `/WhereInTheWorld/country.html?name=${country.name.common}`;
+      countriesCard.innerHTML = `<img src="${country.flags.svg}">
+            <div class="countries-info">
+              <h5 class="countryName">${country.name.common}</h5>
+              <h6 class="population">Population: <span>${country.population.toLocaleString(
+                "en-IN"
+              )}</span></h6>
+              <h6 class="region">Region: <span>${country.region}</span></h6>
+              <h6 class="capital">Capital: <span>${country.capital}</span></h6>
+            </div>`;
+      main.appendChild(countriesCard);
+    });
+  });
+
+
+filter.addEventListener("change", () => {
+  while (main.hasChildNodes()) {
+    main.removeChild(main.firstChild);
+  }
+  fetch(`https://restcountries.com/v3.1/region/${filter.value}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((country) => {
+      const countriesCard = document.createElement("a");
+      countriesCard.classList.add("countries-card");
+      countriesCard.href = `/WhereInTheWorld/country.html?name=${country.name.common}`;
+      countriesCard.innerHTML = `<img src="${country.flags.svg}">
+            <div class="countries-info">
+              <h5 class="countryName">${country.name.common}</h5>
+              <h6 class="population">Population: <span>${country.population.toLocaleString(
+                "en-IN"
+              )}</span></h6>
+              <h6 class="region">Region: <span>${country.region}</span></h6>
+              <h6 class="capital">Capital: <span>${country.capital}</span></h6>
+            </div>`;
+      main.appendChild(countriesCard);
+    });
+  })
+});
+
+
+search.addEventListener('input', (inp) =>{
+const inputValue = (inp.target.value).toLowerCase()
+const countriesCards = document.querySelectorAll('.countries-card')
+countriesCards.forEach((searchCard) => {
+  const countryName = searchCard.querySelector('.countryName').textContent.toLowerCase();
+  if (countryName.includes(inputValue)) {
+    searchCard.style.display = "block";
+  } else {
+    searchCard.style.display = "none";
+  }
+});
+})
